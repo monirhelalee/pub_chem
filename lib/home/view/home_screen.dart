6848f5c13b9f8da8a45ef8ac3440b538dbc26955
monir <mdmonirhelale@gmail.com>
2026-02-04 23:30:12 +0600

    add featured compounds and search UI in home screen

--- lib/home/view/home_screen.dart
+++ lib/home/view/home_screen.dart
@@ -1,27 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:pub_chem/app/utils/constants.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            Icons.home,
-            size: 64,
-          ),
-          SizedBox(height: 16),
-          Text(
-            'Home',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+    return SafeArea(
+      child: Padding(
+        padding: const .all(16),
+        child: Column(
+          crossAxisAlignment: .start,
+          children: [
+            SearchBar(
+              leading: const Icon(Icons.search),
+              hintText: 'Search for compounds',
+              onChanged: (value) {},
+              padding: .all(const .only(left: 16)),
+              elevation: .all(0),
+              readOnly: true,
             ),
-          ),
-        ],
+            const SizedBox(height: 16),
+            const Text(
+              'Featured Compounds',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  childAspectRatio: 1.2,
+                ),
+                itemCount: Constants.featuredCompounds.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final compound = Constants.featuredCompounds[index];
+                  return Card(
+                    elevation: 2,
+                    child: Center(
+                      child: Padding(
+                        padding: const .all(8),
+                        child: Column(
+                          mainAxisAlignment: .center,
+                          spacing: 8,
+                          children: [
+                            const Icon(
+                              Icons.science_outlined,
+                              size: 48,
+                            ),
+                            Text(
+                              compound,
+                              textAlign: .center,
+                              style: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: .w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/theme/theme.dart
+++
@@ -1,3 +0,0 @@
-export 'config/app_theme.dart';
-export 'cubit/theme_cubit.dart';
-export 'models/app_theme_mode.dart';
